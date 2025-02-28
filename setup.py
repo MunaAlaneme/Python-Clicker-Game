c0afad149accc1d5cc35d27c3f19d4f77a235a1a
Muna Alaneme <munachisoalaneme676@gmail.com>
2025-02-28 18:33:23 -0500

    e

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ PyInstaller.__main__.run([
     'main.py',
     '--windowed',
     '--noconsole',
-    '--icon=copilot.ico',
+    #'--icon=copilot.ico',
+    '--icon=copilot.icns',
     '--onefile',
     '--onedir'
 ])
